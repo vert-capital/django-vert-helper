3062915579a0e2b5ea88b6340f850455a532fe3a
Caio Faria <caio.faria@vert-capital.com>
2026-08-25 18:17:17 -0300

    atualiza versão para 1.1.1 e remove dependências desnecessárias no setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README_PATH = BASE_DIR / "README.md"
 
 setup(
     name="django-vert-helper",
-    version="1.1.0",
+    version="1.1.1",
     description="Biblioteca para health checks e acoes operacionais",
     long_description=README_PATH.read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
@@ -17,11 +17,6 @@ setup(
         "django>=5.2.12, <6.0",
         "djangorestframework>=3.15.2, <4.0",
         "django-filter>=24.3, <26.0",
-        "django-rq>=4.1, <5.0",
-        "django-q2>=1.9.0, <2.0",
-        "boto3>=1.42.48, <2.0",
-        "confluent-kafka>=1.9.2, <3.0",
-        "psycopg2>=2.9.10, <3.0",
     ],
     python_requires=">=3.12",
 )
